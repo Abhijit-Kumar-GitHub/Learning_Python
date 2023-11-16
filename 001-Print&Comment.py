bf2ea2350bb474ca353e34913318e17e7dc4e381
Abhijit-Kumar-GitHub <140056029+Abhijit-Kumar-GitHub@users.noreply.github.com>
2023-11-16 15:12:45 +0530

    learning how to comment in python and print

---
+++ 001-Print&Comment.py
@@ -0,0 +1,14 @@
+# everything # is a comment until "enter key is pressed"
+
+# syntax to output is simple --  print("whatever u wanna print ") -- take note that single inverted comma '' can be used
+#                                as well
+#                            -- it prints whatever string is written in between the inverted commas
+
+
+# , I think is used to end the statement ....
+
+print('I love pizza!')
+# print ('It's really delicious')------->>running it will give error
+# that is why we usually use "" because it prevents errors like this
+
+print("It's really delicious")
