d77c137f083124138f79387f70b5caf9dd62812a
FarSpawn <140056029+Abhijit-Kumar-GitHub@users.noreply.github.com>
2023-11-21 22:39:14 +0530

    Add files via upload
    Understanding how input works in Python, how to use various input methods

---
+++ 004-User_Input_Exercises.py
@@ -0,0 +1,22 @@
+
+# input("Enter your name :")
+
+# With this statement the prompt will be displayed in the console window and it will also take the input name
+# but it will not do anything else
+
+# declare the data entered by user as an variable before to store the information and to call it when needed
+
+name = input("Enter your name : ")
+
+# age = input("Enter your age : ")
+# age = int(age)
+
+# Since we are going to perform mathematical operation we need to convert it into float or int data type as user input
+# is always str type
+# We can typecast it into our preferred data type in single line as:
+
+age = int(input("Enter your age : "))
+age = age + 1
+ 
+print(f"Hello {name}.")
+print(f"{name} is going to be {age} years old next year")
