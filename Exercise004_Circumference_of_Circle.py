a58821eeba62503686d44c7139ab22a4a5e0e475
FarSpawn <140056029+Abhijit-Kumar-GitHub@users.noreply.github.com>
2023-11-24 17:02:00 +0530

    Circumference_Calculator
    A simple python program to calculate the circumference of circle by importing the math module

---
+++ Exercise004_Circumference_of_Circle.py
@@ -0,0 +1,12 @@
+import math
+
+radius = float(input("Enter the radius of circle in cm: "))
+
+circumference = 2 * math.pi * radius
+area = math.pi * pow(radius,2)
+
+print(f"The circumference of the circle is: {round(circumference, 2)} cm")
+print(f"The area of the circle is: {round(area, 2)} cm^2")
+
+
+
