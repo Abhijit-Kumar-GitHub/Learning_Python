17b268f7820693cfeadccf9fe04e75db806e2039
FarSpawn <140056029+Abhijit-Kumar-GitHub@users.noreply.github.com>
2023-11-27 19:43:29 +0530

    Hypotenuse_finder[importing_selected_functions]
    A simple python program to find the hypotenuse of a right angle triangle using Pythogora's Theorum and importing just the functions sqrt(square root) and pow(power/exponent) from the module math.

---
+++ Exercise005_Hypotenuse.py
@@ -0,0 +1,8 @@
+import math
+
+h = float(input("Enter the height of the right angled triangle: "))
+b = float(input("Enter the base of the right angled triangle: "))
+
+hypotenuse = math.sqrt(pow(h, 2) + pow(b, 2))
+
+print(f"The hypotenuse of the right angled triangle is: {round(hypotenuse, 2)}")
